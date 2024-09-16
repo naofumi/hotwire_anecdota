f719e2dc8739f9ddc2d14e528d7f02a93c3f3dae
Naofumi Kagami <naofumi@mac.com>
2024-09-16 16:29:10 +0900

    Tiers with Toggle refactor

--- app/javascript/controllers/tiers_with_toggle_controller.js
+++ app/javascript/controllers/tiers_with_toggle_controller.js
@@ -1,13 +1,30 @@
 import {Controller} from "@hotwired/stimulus"
-import {setRadioGroup} from "../utlitilies";
+import get from "lodash/get"
 
+/*
+* Limited Client-side rendering
+* ==========
+*
+* In this pattern, we send in data through the `data-price-table-value` attribute on
+* the controller, and render certain "slots" in the HTML.
+*
+* The data looks like this.
+* We use Lodash `get` to specify which data goes into each slot
+* ```
+* {
+*   "freelancer" => {"monthly" => "$15", "annually" => "$144"},
+*   "startup" => {"monthly" => "$30", "annually" => "$288"},
+*   "enterprise" =>  {"monthly" => "$60", "annually" => "$576"}
+* }
+* ```
+* */
 // Connects to data-controller="tiers-with-toggle"
 export default class extends Controller {
   static values = {frequency: String, priceTable: Object}
-  static targets = ["price", "frequency"]
+  static targets = ["slot", "frequency"]
 
   connect() {
-    setRadioGroup("frequency", this.frequencyValue)
+    this.#render()
   }
 
   setFrequency(event) {
@@ -16,15 +33,25 @@ export default class extends Controller {
   }
 
   #render() {
+    this.#updateFrequencyToggle()
     this.#updatePrices()
+  }
+
+  #updateFrequencyToggle() {
+    document.querySelectorAll(`input[type=radio][name=frequency]`)
+      .forEach(e=> {
+        e.checked = e.value === this.frequencyValue ? true : false
+      })
   }
 
   #updatePrices() {
     const priceTable = this.priceTableValue
 
-    this.priceTargets.forEach(priceTarget => {
-      const plan = priceTarget.dataset.tierPlan
-      priceTarget.textContent = priceTable[plan][this.frequencyValue]
+    this.slotTargets.forEach(slot => {
+      const plan = slot.dataset.tierPath
+      // Use Lodash get (https://lodash.com/docs/4.17.15#get) to
+      // allow flexibile retrieval of data from the `data-tier-path` attribute
+      slot.textContent = get(priceTable, `${plan}.${this.frequencyValue}`)
     })
   }
 }
