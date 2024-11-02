69ba1de7c9feee96c6255f24a6f5447ea6164e02
Naofumi Kagami <naofumi@mac.com>
2024-11-02 17:05:22 +0900

    refactor

--- app/javascript/react_iphone.jsx
+++ app/javascript/react_iphone.jsx
@@ -1,15 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {createRoot} from "react-dom/client";
 import IphoneOption from "./react/components/IphoneOption"
 import IphoneColorOption from "./react/components/IphoneColorOption"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = createRoot(
-    document.body.appendChild(document.createElement("div"))
-  );
   const dataJSON = document.getElementById('catalog-data').textContent
   const data = JSON.parse(dataJSON)
 
+  const root = createRoot(document.getElementById("root"))
   root.render(<IPhone catalogData={data} />);
 });
 
