06ae55ef1ff39e22436de79c366a8f66da7e2887
Naofumi Kagami <naofumi@mac.com>
2024-11-21 15:33:01 +0900

    rename application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "tailwind_rails_production"
+  # config.active_job.queue_name_prefix = "anecdotes_on_hotwire_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
