d3e775f9b79a910773fd309d4b78afc5c90e609c
Naofumi Kagami <naofumi@mac.com>
2024-11-22 16:08:53 +0900

    allow browsers (is google blocked?)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   private
 
