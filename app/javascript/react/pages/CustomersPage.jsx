ca9f2705421519bc2e50e3da7cb61c4df6695de8
Naofumi Kagami <naofumi@mac.com>
2024-11-30 09:31:46 +0900

    fixing react/customers

--- app/javascript/react/pages/CustomersPage.jsx
+++ app/javascript/react/pages/CustomersPage.jsx
@@ -45,14 +45,20 @@ export default function CustomersPage() {
   const [query, setQuery] = useState("")
   const [customers, loading, reloadData] = useServerData(`/customers?query=${query}`, [])
 
+  function navigateTo(customer) {
+    document.location.href = `/customers/${customer.id}/edit?redirect_to=/react/customers`
+  }
+
   return <div className="max-w-lg mx-auto">
     <div className="mb-16">
-      <h1 className="text-4xl text-center">Customers</h1>
+      <h1 className="text-4xl text-center">Customers React</h1>
     </div>
 
     <Search query={query} setQuery={setQuery} loading={loading}/>
 
-    <button onClick={reloadData} type="button" className="btn-primary">Reload</button>
+    <div className="text-right">
+      <button onClick={reloadData} type="button" className="btn-primary">Reload</button>
+    </div>
 
     <table className="table table-striped w-full">
       <thead>
@@ -72,13 +78,13 @@ export default function CustomersPage() {
             {customer.jp_name}
           </td>
           <td className="p-2">
-            <a href={`/react/customers/edit/${customer.id}`}>
+            <button onClick={() => navigateTo(customer)}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                    stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round"
                       d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
               </svg>
-            </a>
+            </button>
           </td>
         </tr>
       ))}
