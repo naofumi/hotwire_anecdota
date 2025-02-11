016e10f4cc2a5bca889fbdee3e8241cdec9e2d9c
Naofumi Kagami <naofumi@mac.com>
2025-02-11 20:12:32 +0900

    Rails 8 migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_16_041304) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_11_110945) do
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type", null: false
     t.integer "trackable_id", null: false
